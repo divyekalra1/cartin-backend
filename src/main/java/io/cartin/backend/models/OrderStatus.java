9246eae3b9913c9879d370f579694ab454e448b9
Divye Kalra <divyekalra1@gmail.com>
2022-12-05 22:45:03 +0530

    order updation function added

--- src/main/java/io/cartin/backend/models/OrderStatus.java
+++ src/main/java/io/cartin/backend/models/OrderStatus.java
@@ -1,7 +1,24 @@
 package io.cartin.backend.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class OrderStatus {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private OrderStatusType statusType;
 
+    public OrderStatus(OrderStatusType statusType) {
+        this.statusType = statusType;
+    } //function for status updation
 
 }
